0cac7a175861d5cc1d2209dfd333fb30531fe94d
Kirill Buglak <kirill.engineer.buglak@gmail.com>
2023-06-10 17:31:26 +0300

    initial commit

---
+++ src/main/java/com/example/transactiosntries/TransactiosnTriesApplication.java
@@ -0,0 +1,13 @@
+package com.example.transactiosntries;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TransactiosnTriesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TransactiosnTriesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/transactiosntries/TransactiosnTriesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.transactiosntries;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TransactiosnTriesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
